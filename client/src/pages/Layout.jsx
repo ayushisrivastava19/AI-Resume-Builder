29e9726136b4783413b951b245f80913c70077f6
Ayushi Srivastava <ayushisrivastava1924@gmail.com>
2026-03-22 20:14:39 +0530

    Initial UI setup: home,login, dashboard started

---
+++ client/src/pages/Layout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import React from 'react'
+import Navbar from "../components/Navbar";
+
+const Layout = () => {
+  return (
+    <div>
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
+          <Outlet /> 
+        </div>
+    </div>
+  );
+};
+
+export default Layout;
