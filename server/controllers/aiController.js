134a806391d63c4c66eebc8682ddd276aeac7c2e
Ayushi Srivastava <ayushisrivastava1924@gmail.com>
2026-03-29 03:02:03 +0530

    Updated gitignore and backend setup with AI + file upload

---
+++ server/controllers/aiController.js
@@ -0,0 +1,54 @@
+
+
+//controller for enhancing a resume's professional summary
+//POST: /api/ai/enhance-pro-sum
+export const enhanceProfessionalSummary = async (req, res) => {
+        try {
+            const{ userContent } = req.body;
+
+            if(!userContent){
+                return res.status(400).json({message: 'Missing required fields'})
+            }
+            const response = await ai.chat.completions.create({
+                model: process.env.OPENAI_MODEL,
+            messages: [
+        {   role: "system", content: "You are an expert in resume writing. Your task is to enhance the professional summary of a resume. The summary should be 1-2 sentences also highlighting key skills, experience, and career objectives. Make it compelling and ATS-friendly. and only return text no options or anything else." },
+        {
+            role: "user", content: userContent, },
+            ],
+            })
+
+            const enhanceContent = response.choices[0].message.content;
+            return res.status(200).json({enhanceContent})
+
+        } catch (error) {
+            return res.status(200).json({message: error.message})
+        }
+}
+
+//controller for enhancing a resume's job description
+//POST: /api/ai/enhance-job-desc
+export const enhanceJobDescription = async (req, res) => {
+        try {
+            const{ userContent } = req.body;
+
+            if(!userContent){
+                return res.status(400).json({message: 'Missing required fields'})
+            }
+            const response = await ai.chat.completions.create({
+                model: process.env.OPENAI_MODEL,
+                messages: [{   role: "system", content: "You are an expert in resume writing. Your task is to enhance the job description of a resume. The job description should be 1-2 sentences also highlighting key responsibilities, and achievements. Use action verbs and quantifiable results where possible. Make it ATS-friendly. and only return text no options or anything else." },
+               {role: "user", content: userContent, },
+                ],
+                 })
+
+            const enhanceContent = response.choices[0].message.content;
+            return res.status(200).json({enhanceContent})
+
+        } catch (error) {
+            return res.status(200).json({message: error.message})
+        }
+}
+
+//controller for uploading a resume to the database
+//POST: /api/ai/upload-resume
